6a7c24041c5b6ea5d29fa8b6969d03ad192e3693
Faoussane <yfaoussane@gmail.com>
2025-09-18 22:59:55 +0100

    ..

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ let batonnets = document.querySelector('.batonnets');
 let nbrjoueurs = document.querySelector('.nbrjoueurs');
 let btn = document.getElementById('btn');
 let nbJoueurs = document.getElementById('nbJoueurs');
+let compteur = document.getElementById('compteur');
 let joueurActuel = 1;
 
+function fucCompteur() {
+    compteur.textContent = batonnets.children.length;
+}
 // le nombre de joueurs ; 1 par defaut
 btn.addEventListener('click', (e)=>{
     e.preventDefault();
@@ -28,15 +32,17 @@ function retirerBatonnets(nbr){
             batonnets.removeChild(batonnets.lastElementChild);
         }
 }
+fucCompteur();
     if (batonnets.children.length ===0 ){
         batonnets.innerHTML=`
         <div>
         <p>Le joueur ${joueurActuel} a perdu !</p>
         <button class="btn4 bn" onclick="recharge()">Rejouer</button>
     </div>`;
+    return;
     }
     if (nbJoueurs ===2){
-        joueurActuel = joueurActuel === 1 ? 2 : 1;
+        joueurActuel = joueurActuel === 1 ? 2 : 1;//opérateur ternaire
         joueur.textContent = "😎Tour du joueur " + joueurActuel;
     }
     else{
@@ -50,11 +56,21 @@ function retirerBatonnets(nbr){
             joueur.textContent = "😎Tour du joueur 1"
         }
     }
+    
 }
 function tourIA(){
-    let choix = Math.min(Math.floor(Math.random()*3)+1, batonnets.children.length);
+    let baton =batonnets.children.length;
+    let choix;
+    if (baton <= 4) {
+        choix = baton - 1;
+        if (choix <= 0) choix = 1; // sécurité
+    }else {
+        choix = Math.floor(Math.random() * 3) + 1;
+        choix = Math.min(choix, baton);
+    }
     retirerBatonnets(choix);
 }
+// utilisé dans le html
 function recharge(){
     location.reload();
 }
